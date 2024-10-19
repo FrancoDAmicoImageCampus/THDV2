11aab2de2e3589e4cb2f314fdc764737ca156dd1
Franco D'Amico <franco.damico@estudiante.imagecampus.edu.ar>
2024-10-19 10:07:53 -0300

    Rework del mapa y agregado de capsula animada

---
+++ My project/Assets/Scripts/SO/PlayerDataSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerDataSO")]
+
+public class PlayerData : ScriptableObject
+{
+	public int jumpSpeed;
+}
